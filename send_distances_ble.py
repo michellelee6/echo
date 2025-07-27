1159dd1d81886055bbad980eab559bcbcaf467db
michellelee6 <112292853+michellelee6@users.noreply.github.com>
2025-07-26 18:42:22 -0700

    Update send_distances_ble.py

--- send_distances_ble.py
+++ send_distances_ble.py
@@ -100,7 +100,6 @@ class DistanceCharacteristic(dbus.service.Object):
             {'Value': self.value},
             []
         )
-        print("🔔 Notifying with value:", self.value)
 
     def update_sensor_value(self):
         readings = []
