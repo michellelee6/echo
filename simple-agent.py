dc32eb938c55321b149357d69276beb5d7bb94ae
michellelee6 <112292853+michellelee6@users.noreply.github.com>
2025-07-28 17:32:24 -0700

    Update simple-agent.py

--- simple-agent.py
+++ simple-agent.py
@@ -14,26 +14,29 @@ class Agent(dbus.service.Object):
 
     @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="")
     def RequestAuthorization(self, device):
-        print("RequestAuthorization", device)
+        print("RequestAuthorization:", device)
 
     @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="s")
     def RequestPinCode(self, device):
-        print("RequestPinCode", device)
+        print("RequestPinCode:", device)
         return "0000"
 
     @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="u")
     def RequestPasskey(self, device):
-        print("RequestPasskey", device)
+        print("RequestPasskey:", device)
         return dbus.UInt32(123456)
 
     @dbus.service.method("org.bluez.Agent1", in_signature="ouq", out_signature="")
     def DisplayPasskey(self, device, passkey, entered):
-        print("DisplayPasskey", device, passkey, entered)
+        print("DisplayPasskey:", device, passkey, entered)
 
     @dbus.service.method("org.bluez.Agent1", in_signature="o", out_signature="")
+    def RequestConfirmation(self, device):
+        print("RequestConfirmation:", device)
+
+    @dbus.service.method("org.bluez.Agent1", in_signature="os", out_signature="")
     def AuthorizeService(self, device, uuid):
-        print("AuthorizeService", device, uuid)
-        return
+        print("AuthorizeService:", device, uuid)
 
     @dbus.service.method("org.bluez.Agent1", in_signature="", out_signature="")
     def Cancel(self):
@@ -41,21 +44,17 @@ class Agent(dbus.service.Object):
 
 def main():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
     bus = dbus.SystemBus()
-    path = "/test/agent"
-    capability = "NoInputNoOutput"
 
-    agent = Agent(bus, path)
+    agent = Agent(bus, AGENT_PATH)
 
-    obj = bus.get_object("org.bluez", "/org/bluez")
-    manager = dbus.Interface(obj, "org.bluez.AgentManager1")
-    manager.RegisterAgent(path, capability)
-    manager.RequestDefaultAgent(path)
+    manager = dbus.Interface(bus.get_object("org.bluez", "/org/bluez"),
+                             "org.bluez.AgentManager1")
+    manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
+    manager.RequestDefaultAgent(AGENT_PATH)
 
-    print("Agent registered")
-    mainloop = GLib.MainLoop()
-    mainloop.run()
+    print("Agent registered and running with NoInputNoOutput")
+    GLib.MainLoop().run()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
